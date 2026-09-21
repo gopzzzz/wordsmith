32e9519c9513cf24d9ba3d4ec99b549fd8254c4f
eldhose <ebiju30@gamil.com>
2026-09-21 16:16:46 +0530

    webbanners

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -3,23 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WebController extends Controller
 {
-    public function index(){
-        return view('web.index');
+    public function index()
+    {
+        // Banner
+        $banner = DB::table('banners')
+            ->orderBy('id', 'asc')
+            ->first();
+
+        // Portfolios
+        $portfolios = DB::table('portfolios')
+            ->orderBy('id', 'asc')
+            ->get();
+
+        return view('web.index', compact('banner', 'portfolios'));
     }
-    public function aboutus(){
-         return view('web.aboutus');
+
+    public function aboutus()
+    {
+        return view('web.aboutus');
     }
-    public function ourservices(){
-         return view('web.services');
+
+    public function ourservices()
+    {
+        return view('web.services');
     }
-    public function ourblogs(){
-          return view('web.blogs');
+
+    public function ourblogs()
+    {
+        return view('web.blogs');
     }
-    public function blogdetails(){
+
+    public function blogdetails()
+    {
         return view('web.blogdetails');
     }
-   
 }
